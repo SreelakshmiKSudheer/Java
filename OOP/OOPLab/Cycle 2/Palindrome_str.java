f138e4f6f00abbab55bc58249e9c086621b75ba3
Sreelakshmi K <119166968+SreelakshmiKSudheer@users.noreply.github.com>
2023-12-03 15:21:17 +0530

    Update Palindrome_str.java

--- OOP/OOPLab/Cycle 2/Palindrome_str.java
+++ OOP/OOPLab/Cycle 2/Palindrome_str.java
@@ -1,63 +1,31 @@
 
 class str {
-    char[] word;
-    int length;
-    boolean palindrome;
-    String wordS;
+    private char[] word;
 
-    public static String toString(char[] a) {
-        String string = new String(a);
-        return string;
+    public str(char[] word) {
+        this.word = word;
     }
 
-    void get_str(char[] str) {
-        word = str;
-        wordS = toString(word);
+    public boolean palin() {
 
-    }
+        int l = 0, r = word.length - 1;
 
-    int len() {
-        int i;
-
-        for (i = 0; word[i] != '\0'; i++)
-            ;
-        this.length = i;
-        return i;
-    }
-
-    boolean palin() {
-        palindrome = true;
-        int l, r;
-
-        l = 0;
-        r = length - 1;
         while (l < r) {
             if (word[l++] != word[r--]) {
-                palindrome = false;
-                break;
+                System.out.println("Not a palindrome");
+                return false;
             }
         }
-
-        return palindrome;
+        System.out.println("Palindrome");
+        return true;
     }
-
-    void display() {
-        if (palindrome)
-            System.out.println(wordS + " is a palindrome");
-        else
-            System.out.println(wordS + " is not a palindrome");
-    }
-
 }
 
 public class Palindrome_str {
     public static void main(String[] arg) {
-        str s1;
-        s1 = new str();
-        char[] s = { 'm', 'a', 'l', 'a', 'y', 'a', 'l', 'a', 'm', '\0' };
-        s1.get_str(s);
-        s1.len();
-        s1.palin();
-        s1.display();
+        char[] s = { 'm', 'a', 'l', 'a', 'y', 'a', 'l', 'a', 'm' };
+
+        str S = new str(s);
+        S.palin();
     }
 }
