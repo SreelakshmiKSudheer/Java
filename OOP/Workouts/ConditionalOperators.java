312ec35db9d445adc562ab4385638b58dc2fcd12
Sreelakshmi K <119166968+SreelakshmiKSudheer@users.noreply.github.com>
2024-01-12 19:19:23 +0530

    Create ConditionalOperators.java

---
+++ OOP/Workouts/ConditionalOperators.java
@@ -0,0 +1,12 @@
+public class ConditionalOperators
+{
+  public static void main(String[] args)
+  {
+    int a = 10,result;
+
+    result = (a == 1)? 1: 10;
+
+    System.out.println(result);
+    
+  }
+}
