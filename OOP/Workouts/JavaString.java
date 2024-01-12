d9c295d0fd888982eb909bff6f162cf61862e708
Sreelakshmi K <119166968+SreelakshmiKSudheer@users.noreply.github.com>
2024-01-12 22:47:19 +0530

    Update JavaString.java

--- OOP/Workouts/JavaString.java
+++ OOP/Workouts/JavaString.java
@@ -5,17 +5,20 @@ public class JavaString {
 
         String firstName = "Sreelakshmi K ";
         String secondName = "Stark";
+        int JerseyNo = 35;
+        String reveal = "My Jersey No. is ";
 
         System.out.println(s.length());
         System.out.println(s.toUpperCase());
         System.out.println(s.toLowerCase());
-        System.out.println(s.indexOf('e'));
+        System.out.println(s.indexOf('e'));     // index of first occurance of 'e'
 
         s = s + " World";
 
         System.out.println(s);
 
-        System.out.println(firstName.concat(secondName));
+        System.out.println("I am " + firstName.concat(secondName));
+        System.out.println(reveal + JerseyNo);                          // str + int -> concat
 
     }
 }
