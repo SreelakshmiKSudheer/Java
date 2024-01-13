a75d0bac6f92e673e4e26d85e6ce40b8e950709f
Sreelakshmi K <119166968+SreelakshmiKSudheer@users.noreply.github.com>
2024-01-13 23:34:47 +0530

    Add files via upload

---
+++ OOP/Workouts/Consolewrite.java
@@ -0,0 +1,10 @@
+public class Consolewrite {
+    public static void main(String[] args) {
+        int value;
+
+        value = 'X';
+
+        System.out.write(value);
+        System.out.write('\n');
+    }
+}
