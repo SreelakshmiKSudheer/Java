7aab2557d05834cb848688e4a67ec18f87d6a6e8
Sreelakshmi K <119166968+SreelakshmiKSudheer@users.noreply.github.com>
2024-01-14 10:57:58 +0530

    Create JavaString1.java

---
+++ OOP/Workouts/JavaString1.java
@@ -0,0 +1,15 @@
+public class JavaString
+{
+  public static void main(String[] args)
+  {
+    String s1 = "Hello";
+    String s2 = new String("World");
+    char[] ch = {'R','e','a','d','y'};
+    String s3 = new String(ch);
+    
+    System.out.println(s1);
+    System.out.println(s2);
+    System.out.println(s3);
+    
+  }
+}
